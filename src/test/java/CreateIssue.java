545b1857a761d9e78856f0d906b93e343c6738d3
QCBee <olgalyaschevskaya@gmail.com>
2020-07-21 11:55:20 +0300

    Added cancel flow test for create issue class

--- src/test/java/CreateIssue.java
+++ src/test/java/CreateIssue.java
@@ -38,6 +38,7 @@ public class CreateIssue {
     String summaryValueOnViewTicketLocator = "//h1[contains(text(),'Test Summary')]";
     String reporterValueOnViewJiraTicketLocator = "[alt = 'webinar5']";
     String typeIssueValueOnViewJiraTicketLocator = "[title='Task - A task that needs to be done.']";
+    String cancelButtonLocator = "[class = 'cancel']";
 
     public CreateIssue() {
     }
@@ -104,8 +105,8 @@ public class CreateIssue {
         boolean doProjectValueMatch = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(projectValueOnViewTicketLocator))).isDisplayed();
         Assert.assertEquals(doProjectValueMatch,true);
 
-        boolean doSummaruValueMatch = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(summaryValueOnViewTicketLocator))).isDisplayed();
-        Assert.assertEquals(doSummaruValueMatch,true);
+        boolean doSummaryValueMatch = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(summaryValueOnViewTicketLocator))).isDisplayed();
+        Assert.assertEquals(doSummaryValueMatch,true);
 
         boolean doTypeIssueValueMatch = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(typeIssueValueOnViewJiraTicketLocator))).isDisplayed();
         Assert.assertEquals(doTypeIssueValueMatch,true);
@@ -113,8 +114,48 @@ public class CreateIssue {
         boolean doReporterValueMatch = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(reporterValueOnViewJiraTicketLocator))).isDisplayed();
         Assert.assertEquals(doReporterValueMatch,true);
     }
+
+    @Test
+    public void cancelCreatingNewJiraTicket(){
+        driver.findElement(By.id(createIssueButtonOnDashboardLocator)).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(70).getSeconds());
+        boolean createIssuePopUpIsPresent = (wait.until(ExpectedConditions.presenceOfElementLocated(By.id(createIssuePopPupLocator)))).isEnabled();
+        Assert.assertEquals(createIssuePopUpIsPresent, true);
+
+        driver.findElement(By.id(projectInputLocator)).clear();
+        driver.findElement(By.id(projectInputLocator)).sendKeys(projectNameValue);
+        driver.findElement(By.id(projectInputLocator)).sendKeys(Keys.ENTER);
+        driver.findElement(By.id(issueTypeInputLocator)).isEnabled();
+        driver.findElement(By.id(issueTypeInputLocator)).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.findElement(By.id(issueTypeInputLocator)).clear();
+        driver.findElement(By.id(issueTypeInputLocator)).sendKeys(issueTypeValue);
+        driver.findElement(By.id(issueTypeInputLocator)).sendKeys(Keys.ENTER);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(summaryInputLocator))).isEnabled();
+        driver.findElement(By.id(summaryInputLocator)).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.findElement(By.id(summaryInputLocator)).sendKeys(summaryValue);
+        driver.findElement(By.id(reporterInputLocator)).clear();
+        driver.findElement(By.id(reporterInputLocator)).sendKeys(reporterValue);
+        driver.findElement(By.id(reporterInputLocator)).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector(cancelButtonLocator)).click();
+        //TODO Add navigation in browser window
+
+    }
     //TODO Add test for successful create flow for all fields (non required + required)
-    //TODO Add test for cancel flow for creation
     //TODO Add test for unsuccessful flow (validation for required fields)
 
     @AfterMethod
